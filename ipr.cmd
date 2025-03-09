862e2650be0bd9b6590fa75835eab7aa30b3a9a6
DanEdens <d.edens@elementalmachines.com>
2025-03-08 19:46:26 -0500

    update ip remote retrival script

--- ipr.cmd
+++ ipr.cmd
@@ -1,2 +1,3 @@
 REM Pub public ipv4 address to topic
-ipconfig|grep -m 1 IPv4|sed "s/IPv4 Address. . . . . . . . . . . : //g"|sed 's/   //g'| xargs -t -I {} rost.cmd status/%DENA%/IP {}
+curl https://api.ipify.org |xargs -I {} "C:\Program Files\mosquitto\mosquitto_pub.exe" -h %awsip% -p %awsport% -t status/%DeNa%/ip -m {}
+REM ipconfig|grep -m 1 IPv4|sed "s/IPv4 Address. . . . . . . . . . . : //g"|sed 's/   //g'| xargs -t -I {} rost.cmd status/%DENA%/IP {}
